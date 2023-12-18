b2612c267d697ea2c1a6131c07f0a822e7e0b0fe
ucynthia13 <cycyu614@gmail.com>
2023-12-18 12:02:42 +0200

    ...

--- src/test/java/com/example/devops/controllerTest/MathControllerEndToEndTest.java
+++ src/test/java/com/example/devops/controllerTest/MathControllerEndToEndTest.java
@@ -1,37 +1,30 @@
 package com.example.devops.controllerTest;
 
 import com.example.devops.dto.DoMathRequest;
-import com.example.devops.exceptions.InvalidOperationException;
-import com.example.devops.service.MathOperator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-
-// End-to-End Test
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MathControllerEndToEndTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @MockBean
-    private MathOperator mathOperator;
+    @LocalServerPort
+    private int port;
 
     @Test
     public void testDoMath_Success() throws Exception {
         // Arrange
         DoMathRequest request = new DoMathRequest(5, 4, "*");
-        when(mathOperator.doMath(5, 4, "*")).thenReturn(20.0);
 
         // Act
         ResponseEntity<String> responseEntity = restTemplate.postForEntity("/api/math/doMath", request, String.class);
@@ -45,7 +38,6 @@ public class MathControllerEndToEndTest {
     public void testDoMath_DivideByZero() throws Exception {
         // Arrange
         DoMathRequest request = new DoMathRequest(5, 0, "/");
-        when(mathOperator.doMath(5, 0, "/")).thenThrow(new InvalidOperationException("Cannot divide by 0"));
 
         // Act
         ResponseEntity<String> responseEntity = restTemplate.postForEntity("/api/math/doMath", request, String.class);
@@ -59,7 +51,6 @@ public class MathControllerEndToEndTest {
     public void testDoMath_UnknownOperation() throws Exception {
         // Arrange
         DoMathRequest request = new DoMathRequest(5, 4, "%");
-        when(mathOperator.doMath(5, 4, "%")).thenThrow(new RuntimeException("Unknown operation"));
 
         // Act
         ResponseEntity<String> responseEntity = restTemplate.postForEntity("/api/math/doMath", request, String.class);
